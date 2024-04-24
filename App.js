d490d59fbdbc6ae1b9160528f054b176ebc253e0
iremserenay <irem@serenay.net.tr>
2024-04-24 15:43:54 +0300

    making items deletable and using IDs

--- App.js
+++ App.js
@@ -13,8 +13,10 @@ export default function App() {
     ]);
   }
 
-  function deleteGoalHandler() {
-    console.log("Delete");
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
@@ -27,6 +29,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDelete={deleteGoalHandler}
               />
             );
